db2d44926f076c6776c6655ab6d5971cbe37c171
Christian Belloni <christian.belloni@babyloncloud.com>
2026-01-22 16:48:49 +0100

    .

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -17,7 +17,9 @@ SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 # Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
 docs="$(mktemp -d)"; targets="$(mktemp)"
-bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+
+# examples dir breaks docs extraction TODO: fix this
+# bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
 bazel --output_base="$docs" build --target_pattern_file="$targets"
 tar --create --auto-compress \
     --directory "$(bazel --output_base="$docs" info bazel-bin)" \
